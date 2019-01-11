f2f2da2a39f517d4d6e8ebafd66d1244ac1bfa5a
Timothy J. Carter <tcarter@crossroadsnet.com>
2019-01-11 09:14:07 -0700

    section 2, lecture 9

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,45 @@
-console.log('Hello World!!!')
+/**** 
+ * Variables and Data types
+ */
+
+ /*
+var firstName = 'John'
+var lastName = 'Smith'
+var age = 28
+console.log(firstName)
+
+var fullAge = true
+console.log(fullAge)
+
+var job
+console.log(job)
+
+job = 'teacher'
+console.log(job)
+
+// Variable naming rules
+var $3years = 3
+var _3years = 3
+var threeYears = 3
+*/
+
+var firstName = 'John'
+var age = 28
+
+// Type Coercion
+console.log(firstName + ' ' + age)
+
+var job, isMarried
+
+job = 'teacher'
+isMarried = false
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried)
+
+// Variable Mutation
+age = 'twenty eight'
+job = 'driver'
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried)
+
+var lastName = prompt('What is his last name?')
+console.log(firstName + ' ' + lastName)
